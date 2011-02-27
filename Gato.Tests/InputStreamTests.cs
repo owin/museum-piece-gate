c6aa6e79c45e87ccb678778456369c928379e9d2
Benjamin van der Veen <bvanderveen@bvanderveen.local>
2011-02-26 17:47:20 -0800

    less

---
+++ Gato.Tests/InputStreamTests.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using NUnit.Framework;
+
+namespace Gato.Tests
+{
+    [TestFixture]
+    public class InputStreamTests
+    {
+        ArraySegment<byte> ArrSeg(string str)
+        {
+            return new ArraySegment<byte>(Encoding.UTF8.GetBytes(str));
+        }
+
+        string SynchronousReadToEndOrException(ref Exception exception)
+        {
+            byte[] buffer = new byte[1024];
+            StringBuilder sb = new StringBuilder();
+
+            while (true)
+            {
+                int bytesRead = 0;
+
+                try
+                {
+                    bytesRead = stream.Read(buffer, 0, buffer.Length);
+                }
+                catch (Exception e)
+                {
+                    exception = e;
+                    break;
+                }
+
+                if (bytesRead == 0)
+                    break;
+
+                sb.Append(Encoding.ASCII.GetString(buffer, 0, bytesRead));
+            }
+
+            return sb.ToString();
+        }
+
+        Stream stream;
+
+        void CreateStream(Func<IObserver<Tuple<ArraySegment<byte>, Action>>, Action> observable)
+        {
+            stream = new ObservableInputStream(new Observable<Tuple<ArraySegment<byte>, Action>>(observable));
+        }
+
+        [Test]
+        public void PsCs()
+        {
+            CreateStream(o =>
+            {
+                o.OnNext(Tuple.Create(ArrSeg("asdf"), (Action)null));
+                o.OnNext(Tuple.Create(ArrSeg("jkl;"), (Action)null));
+                o.OnNext(Tuple.Create(ArrSeg("lol"), (Action)null));
+                o.OnCompleted();
+                return () => { };
+            });
+
+            Exception e = null;
+            var result = SynchronousReadToEndOrException(ref e);
+            Assert.IsNull(e);
+            Assert.AreEqual("asdfjkl;lol", result);
+        }
+
+        [Test]
+        public void PsCsException()
+        {
+            CreateStream(o =>
+            {
+                o.OnNext(Tuple.Create(ArrSeg("asdf"), (Action)null));
+                o.OnNext(Tuple.Create(ArrSeg("jkl;"), (Action)null));
+                o.OnNext(Tuple.Create(ArrSeg("lol"), (Action)null));
+                o.OnError(new Exception("ack!"));
+                return () => { };
+            });
+
+            Exception e = null;
+            var result = SynchronousReadToEndOrException(ref e);
+            Assert.IsNotNull(e);
+            Assert.AreEqual("ack!", e);
+            Assert.AreEqual("asdfjkl;lol", result);
+        }
+
+        [Test]
+        public void PaCs()
+        {
+            CreateStream(o =>
+            {
+                o.OnNext(Tuple.Create(ArrSeg("asdf"), (Action)(() => {
+                    o.OnNext(Tuple.Create(ArrSeg("jkl;"), (Action)(() => {
+                        o.OnNext(Tuple.Create(ArrSeg("lol"), (Action)(() => {
+                            o.OnCompleted();
+                        })));
+                    })));
+                })));
+                return () => { };
+            });
+
+            Exception e = null;
+            var result = SynchronousReadToEndOrException(ref e);
+            Assert.IsNull(e);
+            Assert.AreEqual("asdfjkl;lol", result);
+        }
+
+        [Test]
+        public void PaCsException()
+        {
+            CreateStream(o => {
+                o.OnNext(Tuple.Create(ArrSeg("asdf"), (Action)(() => {
+                    o.OnNext(Tuple.Create(ArrSeg("jkl;"), (Action)(() => {
+                        o.OnNext(Tuple.Create(ArrSeg("lol"), (Action)(() => {
+                            o.OnError(new Exception("ack!"));
+                        })));
+                    })));
+                })));
+                return () => { };
+            });
+
+            Exception e = null;
+            var result = SynchronousReadToEndOrException(ref e);
+            Assert.IsNotNull(e);
+            Assert.AreEqual("ack!", e);
+            Assert.AreEqual("asdfjkl;lol", result);
+        }
+    }
+}
---
+++ Gato/Extensions.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gato
+{
+    public static class Extensions
+    {
+        public static string GetRequestMethod(this IDictionary<string, object> env)
+        {
+            return env["owin.RequestMethod"] as string;
+        }
+
+        public static void SetRequestMethod(this IDictionary<string, object> env, string value)
+        {
+            env["owin.RequestMethod"] = value;
+        }
+
+        public static string GetRequestUri(this IDictionary<string, object> env)
+        {
+            return env["owin.RequestUri"] as string;
+        }
+
+        public static void SetRequestUri(this IDictionary<string, object> env, string value)
+        {
+            env["owin.RequestUri"] = value;
+        }
+
+        public static IDictionary<string, string> GetRequestHeaders(this IDictionary<string, object> env)
+        {
+            return env["owin.RequestHeaders"] as IDictionary<string, string>;
+        }
+
+        public static void SetRequestHeaders(this IDictionary<string, object> env, IDictionary<string, string> value)
+        {
+            env["owin.RequestHeaders"] = value;
+        }
+
+        public static string GetRequestHeader(this IDictionary<string, object> env, string name)
+        {
+            var headers = env.GetRequestHeaders();
+            return headers.ContainsKey(name) ? headers[name] : null;
+        }
+
+        public static Func<Func<ArraySegment<byte>, Action, bool>, Action<Exception>, Action, Action>
+            GetRequestBody(this IDictionary<string, object> env)
+        {
+            return env["gato.RequestBody"] as Func<Func<ArraySegment<byte>, Action, bool>, Action<Exception>, Action, Action>;
+        }
+
+        public static void SetRequestBody(this IDictionary<string, object> env, Func<Func<ArraySegment<byte>, Action, bool>, Action<Exception>, Action, Action> value)
+        {
+            env["gato.RequestBody"] = value;
+        }
+    }
+}
