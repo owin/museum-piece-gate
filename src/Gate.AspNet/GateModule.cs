d5a4bf2b5e11c95ae8ba7e2ded4d667b7eecf6ae
Louis DeJardin <louis.dejardin@gmail.com>
2011-03-11 23:03:07 -0800

    Empty harness

---
+++ src/Gate.AspNet/GateModule.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Web;
+
+namespace Gate.AspNet
+{
+    public class GateModule : IHttpModule
+    {
+        public void Init(HttpApplication context)
+        {
+            context.AddOnBeginRequestAsync((sender, args, callback, state) => default(IAsyncResult), ar => { });
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
