46a71f4fdaeac86af62f4ab75a46feac882b6c8b
Louis DeJardin <louis.dejardin@gmail.com>
2011-04-20 11:38:21 -0700

    Adding a FakeApp for easier unit testing

---
+++ src/Tests/Gate.TestHelpers/FakeApp.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Nancy.Hosting.Owin.Tests.Fakes;
+
+namespace Gate.TestHelpers
+{
+    
+    public class FakeApp
+    {
+        public FakeApp()
+        {
+            Headers = new Dictionary<string, string>();
+        }
+
+        public FakeApp(string status, string body)
+        {
+            Status = status;
+            Headers = new Dictionary<string, string>();
+            var buffer = Encoding.UTF8.GetBytes(body);
+            Body = new FakeProducer(false, buffer, 5, true);
+        }
+
+        public FakeApp(Exception faultException)
+        {
+            FaultException = faultException;
+        }
+
+        /// <summary>
+        /// Indicates if AppDelegate method was called
+        /// </summary>
+        public bool AppDelegateInvoked{get;private set;}
+
+        /// <summary>
+        /// Determines the status that will be passed to result delegate by Call
+        /// </summary>
+        public string Status { get; set; }
+
+        /// <summary>
+        /// Determines the response headers that will be passed to result delegate by Call
+        /// </summary>
+        public IDictionary<string, string> Headers { get; set; }
+
+        /// <summary>
+        /// Determines the response body that will be passed to result delegate by Call
+        /// </summary>
+        public FakeProducer Body { get; set; }
+
+        /// <summary>
+        /// If not null, this Exception is passed to fault instead of 
+        /// </summary>
+        public Exception FaultException { get; set; }
+
+        /// <summary>
+        /// Gets the most recent environment passed to Call
+        /// </summary>
+        public IDictionary<string, object> Env { get; private set; }
+
+         /// <summary>
+        /// Gets an Owin property adapter arount the most recent environment
+        /// </summary>
+        public Owin Owin { get {return new Owin(Env);} }
+
+
+        /// <summary>
+        /// The actual app delegate
+        /// </summary>
+        /// <param name="env"></param>
+        /// <param name="result"></param>
+        /// <param name="fault"></param>
+        public void AppDelegate(
+            IDictionary<string, object> env, 
+            Action<string, IDictionary<string, string>, Func<Func<ArraySegment<byte>, Action, bool>, Action<Exception>, Action, Action>> result,
+            Action<Exception> fault)
+        {
+            AppDelegateInvoked = true;
+            Env = env;
+            if (FaultException != null)
+            {
+                fault(FaultException);
+            }
+            else
+            {
+                result(Status, Headers, Body.BodyDelegate);
+            }
+        }
+    }
+}
