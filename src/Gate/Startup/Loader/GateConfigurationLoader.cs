3a5491a97bb5ff6642ffcf8bcaecd32a582b8eb4
Benjamin van der Veen <b@bvanderveen.com>
2011-06-07 09:06:39 -0700

    hide DotByDot

--- src/Gate/Startup/Loader/GateConfigurationLoader.cs
+++ src/Gate/Startup/Loader/GateConfigurationLoader.cs
@@ -131,7 +131,7 @@ namespace Gate
             }
         }
 
-        public static IEnumerable<string> DotByDot(string text)
+        static IEnumerable<string> DotByDot(string text)
         {
             if (text == null)
                 yield break;
