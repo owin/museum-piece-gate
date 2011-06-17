52296241f9ab9f41248608faf7d08d83b735c041
Benjamin van der Veen <b@bvanderveen.com>
2011-06-17 12:36:21 -0700

    Added Kayak Host

---
+++ src/Hosts/Gate.Kayak/DataProducer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Kayak;
+
+namespace Gate.Kayak
+{
+    class DataProducer : IDataProducer
+    {
+        BodyDelegate del;
+        public DataProducer(BodyDelegate del)
+        {
+            this.del = del;
+        }
+
+        public IDisposable Connect(IDataConsumer channel)
+        {
+            return new Disposable(del(
+                (data, continuation) => channel.OnData(data, continuation),
+                error => channel.OnError(error),
+                () => channel.OnEnd()));
+        }
+    }
+}
