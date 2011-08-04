c2b0cc8815d1512cfef7a178ff55b2a35921401b
Benjamin van der Veen <b@bvanderveen.com>
2011-08-04 00:04:58 -0700

    added scheduler middleware to make possibly-multi-threaded wrapped apps play nicely with kayak's scheduler.
    caveats:
     - canceling the request body is a no-op.
     - response body cannot apply back-pressure.

---
+++ src/Hosts/Gate.Kayak/SchedulerMiddleware.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Kayak;
+
+namespace Gate.Kayak
+{
+    public static class AppBuilderExtensions
+    {
+        public static IAppBuilder RescheduleCallbacks(this IAppBuilder builder)
+        {
+            return builder.RescheduleCallbacks(null);
+        }
+
+        public static IAppBuilder RescheduleCallbacks(this IAppBuilder builder, IScheduler scheduler)
+        {
+            return builder.Use(del => new RescheduleCallbacksMiddleware(del, scheduler).Invoke);
+        }
+
+        class RescheduleCallbacksMiddleware
+        {
+            AppDelegate wrapped;
+            IScheduler scheduler;
+
+            public RescheduleCallbacksMiddleware(AppDelegate wrapped, IScheduler scheduler)
+            {
+                this.wrapped = wrapped;
+                this.scheduler = scheduler;
+            }
+
+            public void Invoke(IDictionary<string, object> envDict, ResultDelegate result, Action<Exception> error)
+            {
+                var env = (envDict as Environment ?? new Environment(envDict));
+                var theScheduler = scheduler ?? (IScheduler)env["kayak.Scheduler"];
+
+                var oldBody = env.Body;
+
+                if (oldBody != null)
+                    env.Body = (onNext, onError, onComplete) =>
+                    {
+                        theScheduler.Post(() =>
+                        {
+                            oldBody(
+                                (data, ack) => onNext(data, () => scheduler.Post(ack)), 
+                                onError, 
+                                onComplete);
+                        });
+
+                        // you're giving up your ability to sever the input stream. sucks for you.
+                        return () => { };
+                    };
+
+                wrapped(env, (status, headers, body) => {
+                    theScheduler.Post(() => result(status, headers, (onNext, onError, onComplete) =>
+                        {
+                            return body(
+                                (data, _) => { 
+                                    // you're giving up your ability to limit output buffer size. sucks for you.
+                                    theScheduler.Post(() => onNext(data, null));
+                                    return false;
+                                },
+                                bodyError => theScheduler.Post(() => onError(bodyError)),
+                                () => theScheduler.Post(() => onComplete()));
+                        }));
+                },
+                e => {
+                    theScheduler.Post(() => error(e));
+                });
+            }
+        }
+    }
+}
