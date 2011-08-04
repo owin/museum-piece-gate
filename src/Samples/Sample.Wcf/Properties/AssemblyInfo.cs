db8d0623bcf72c7bb79d980f71df56544ce1f964
Benjamin van der Veen <b@bvanderveen.com>
2011-08-04 00:22:01 -0700

    auto-generated AssemblyInfo.cs files from build

--- src/Samples/Sample.Wcf/Properties/AssemblyInfo.cs
+++ src/Samples/Sample.Wcf/Properties/AssemblyInfo.cs
@@ -1,40 +1,10 @@
-﻿using System.Reflection;
 using System.Runtime.CompilerServices;
+using System.Reflection;
 using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-
 [assembly: AssemblyTitle("Sample.Wcf")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Sample.Wcf")]
-[assembly: AssemblyCopyright("")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("0.1.0")]
+[assembly: AssemblyFileVersion("0.1.0")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
+[assembly: InternalsVisibleTo("Sample.Wcf.Tests")]
 
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-
-[assembly: Guid("75c32928-69a1-4bfd-9bdc-026441169f74")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
