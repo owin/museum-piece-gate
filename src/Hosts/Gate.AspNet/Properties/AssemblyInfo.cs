4e1dfd7e664a0150b2714c92be97141046c8e139
Benjamin van der Veen <b@bvanderveen.com>
2011-08-15 00:37:45 -0700

    bump to v0.1.1

--- src/Hosts/Gate.AspNet/Properties/AssemblyInfo.cs
+++ src/Hosts/Gate.AspNet/Properties/AssemblyInfo.cs
@@ -3,8 +3,8 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Gate.AspNet")]
 [assembly: AssemblyProduct("Gate.AspNet")]
-[assembly: AssemblyVersion("0.1.0")]
-[assembly: AssemblyFileVersion("0.1.0")]
+[assembly: AssemblyVersion("0.1.1")]
+[assembly: AssemblyFileVersion("0.1.1")]
 
 [assembly: InternalsVisibleTo("Gate.AspNet.Tests")]
 
