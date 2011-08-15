4fe3165fa9d537b60984b238bf97d904169e645a
Benjamin van der Veen <b@bvanderveen.com>
2011-08-15 08:53:57 -0700

    bump to v0.1.2

--- src/Hosts/Gate.Wcf/Properties/AssemblyInfo.cs
+++ src/Hosts/Gate.Wcf/Properties/AssemblyInfo.cs
@@ -3,8 +3,8 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Gate.Wcf")]
 [assembly: AssemblyProduct("Gate.Wcf")]
-[assembly: AssemblyVersion("0.1.1")]
-[assembly: AssemblyFileVersion("0.1.1")]
+[assembly: AssemblyVersion("0.1.2")]
+[assembly: AssemblyFileVersion("0.1.2")]
 
 [assembly: InternalsVisibleTo("Gate.Wcf.Tests")]
 
