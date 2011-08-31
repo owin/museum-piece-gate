fe757f156a5222f3b0cd81b874cc4620d6433c9c
Louis DeJardin <louis.dejardin@gmail.com>
2011-08-30 20:04:59 -0700

    Add a Gate.AspNet.Handler class to allow normal pipeline to execute
    Also Adds an AppHandlerSingleton class to control creation/holding
    of AppDelegate for both Module and Handler execution paths

---
+++ src/Hosts/Gate.AspNet/Handler.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Gate.AspNet
+{
+    public class Handler : IHttpAsyncHandler
+    {
+        public IAsyncResult BeginProcessRequest(HttpContext context, AsyncCallback cb, object extraData)
+        {
+            var appHandler = AppHandlerSingleton.Instance;
+            var task = Task.Factory.FromAsync(appHandler.BeginProcessRequest, appHandler.EndProcessRequest, new HttpContextWrapper(context), extraData);
+            if (cb != null)
+                task.ContinueWith(t => cb(t), TaskContinuationOptions.PreferFairness);
+
+            return task;
+        }
+
+        public void EndProcessRequest(IAsyncResult result)
+        {
+            ((Task)result).Wait();
+        }
+
+        public bool IsReusable
+        {
+            get { return true; }
+        }
+
+        public void ProcessRequest(HttpContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- src/Hosts/Gate.AspNet/Module.cs
+++ src/Hosts/Gate.AspNet/Module.cs
@@ -1,42 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.Specialized;
-using System.Configuration;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Web;
-using Gate;
+﻿using System.Web;
 
 namespace Gate.AspNet
 {
     public class Module : IHttpModule
     {
-        private static AppHandler _appHandler;
-
         public void Dispose()
         {
         }
 
         public void Init(HttpApplication init)
         {
-            lock(typeof(Module))
-            {
-                if (_appHandler == null)
-                {
-                    var configurationString = ConfigurationManager.AppSettings["Gate.Startup"];
-                    var app = AppBuilder.BuildConfiguration(configurationString);
-                    _appHandler = new AppHandler(app);
-                }
-            }
-
+            var appHandler = AppHandlerSingleton.Instance;
+            
             init.AddOnBeginRequestAsync(
                 (sender, args, callback, state) =>
                 {
                     var httpContext = ((HttpApplication) sender).Context;
-                    return _appHandler.BeginProcessRequest(new HttpContextWrapper(httpContext), callback, state);
+                    return appHandler.BeginProcessRequest(new HttpContextWrapper(httpContext), callback, state);
                 },
-                _appHandler.EndProcessRequest);
+                appHandler.EndProcessRequest);
         }
-
     }
 }
