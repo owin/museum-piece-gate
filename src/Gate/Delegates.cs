9b0916167daf4f02cff362339e9724ec06be7476
Louis DeJardin <louis.dejardin@gmail.com>
2011-10-30 21:40:49 -0700

    Adding additional assemblies to refactor class divisions
    Moves strongly-typed delegates into Gate.Owin as a first step

--- src/Gate/Delegates.cs
+++ src/Gate/Delegates.cs
@@ -27,26 +27,6 @@ namespace Gate
         Action, //complete
         Action>; //cancel
 
-    public delegate void AppDelegate(
-        IDictionary<string, object> env,
-        ResultDelegate result,
-        Action<Exception> fault);
-
-    public delegate void ResultDelegate(
-        string status,
-        IDictionary<string, string> headers,
-        BodyDelegate body);
-
-    public delegate Action /* cancel */ BodyDelegate(
-        Func<
-            ArraySegment<byte>, // data
-            Action, // continuation
-            bool> // continuation was or will be invoked
-            next,
-        Action<Exception> error,
-        Action complete);
-
-
 
     public static class Delegates
     {
