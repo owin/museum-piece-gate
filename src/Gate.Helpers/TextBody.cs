7f744d6e5c29f8aa702ae2bfa4f643ac434a1d39
Kevin Swiber <kevinswiber@quickenloans.com>
2011-11-09 20:35:15 -0500

    Added Static middleware and supporting classes.

---
+++ src/Gate.Helpers/TextBody.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Text;
+using Gate.Owin;
+
+namespace Gate.Helpers
+{
+    public class TextBody
+    {
+        private readonly string text;
+        private readonly Encoding encoding;
+        private BodyStream bodyStream;
+
+        public TextBody(string text, Encoding encoding)
+        {
+            this.text = text;
+            this.encoding = encoding;
+        }
+
+        public static BodyDelegate Create(string text, Encoding encoding)
+        {
+            return (data, error, complete) =>
+            {
+                var textBody = new TextBody(text, encoding);
+
+                return textBody.Start(data, error, complete);
+            };
+        }
+
+        public Action Start(Func<ArraySegment<byte>, Action, bool> data, Action<Exception> error, Action complete)
+        {
+            bodyStream = new BodyStream(data, error, complete);
+
+            Action start = () =>
+            {
+                try
+                {
+                    if (bodyStream.CanSend())
+                    {
+                        var bytes = encoding.GetBytes(text);
+                        var segment = new ArraySegment<byte>(bytes);
+
+                        // Not buffered.
+                        bodyStream.SendBytes(segment, null, null);
+
+                        bodyStream.Finish();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    bodyStream.Error(ex);
+                }
+            };
+
+            bodyStream.Start(start, null);
+
+            return bodyStream.Cancel;
+        }
+    }
+}
---
+++ src/Gate.Helpers/Utils/HttpFormatExtensions.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Web;
+
+namespace Gate.Helpers.Utils
+{
+    public static class HttpFormatExtensions
+    {
+        public static string ToHttpDateString(this DateTime date)
+        {
+            // Sun, 06 Nov 1994 08:49:37 GMT
+            return date.ToUniversalTime().ToString("R");
+        }
+        public static string Escape(this string uri)
+        {
+            return HttpUtility.UrlEncode(uri);
+        }
+
+        public static string Unescape(this string uri)
+        {
+            return HttpUtility.UrlDecode(uri);
+        }
+    }
+}
