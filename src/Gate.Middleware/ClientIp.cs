548ec079dfd1a5799532aac9bed981bfbdd45e5f
Louis DeJardin <louis.dejardin@gmail.com>
2011-11-09 20:32:36 -0800

    Middleware to normalization and remove known incoming-proxy addrs
    Normalizes use of server.CLIENT_IP and X-Forwarded-For
    Removes known http-proxies, enabling reliable use of either
    of the above for client address blocking or logging

---
+++ src/Gate.Middleware/ClientIp.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Gate.Owin;
+
+namespace Gate.Middleware
+{
+    public static class ClientIp
+    {
+        public static IAppBuilder UseClientIp(this IAppBuilder builder, params string[] knownHttpProxies)
+        {
+            return builder.Use(Middleware, knownHttpProxies);
+        }
+
+        public static AppDelegate Middleware(AppDelegate app, IEnumerable<string> knownHttpProxies)
+        {
+            return (env, result, fault) =>
+            {
+                var req = new Request(env);
+                var clientIp = GetClientIp(req);
+                var forwardedFor = GetForwardedFor(req);
+
+                for (; ; )
+                {
+                    if (clientIp != null && knownHttpProxies.Contains(clientIp))
+                        clientIp = null;
+
+                    if (clientIp != null)
+                        break;
+
+                    if (forwardedFor == null)
+                        break;
+
+                    var finalDelimiter = forwardedFor.LastIndexOfAny(new[] { '\r', '\n', ',' });
+                    clientIp = forwardedFor.Substring(finalDelimiter + 1).Trim();
+                    forwardedFor = finalDelimiter < 1 ? null : forwardedFor.Substring(0, finalDelimiter).Trim();
+                }
+
+                req["server.CLIENT_IP"] = clientIp;
+                if (string.IsNullOrWhiteSpace(forwardedFor))
+                {
+                    if (req.Headers.ContainsKey("X-Forwarded-For"))
+                        req.Headers.Remove("X-Forwarded-For");
+                }
+                else
+                {
+                    req.Headers["X-Forwarded-For"] = forwardedFor;
+                }
+                app(env, result, fault);
+            };
+        }
+
+        static string GetClientIp(IDictionary<string, object> req)
+        {
+            object value;
+            if (req.TryGetValue("server.CLIENT_IP", out value))
+            {
+                var clientIp = Convert.ToString(value);
+                if (!string.IsNullOrWhiteSpace(clientIp))
+                    return clientIp;
+            }
+            return null;
+        }
+
+        static string GetForwardedFor(Request req)
+        {
+            var headers = req.Headers;
+            if (headers == null)
+                return null;
+
+            string forwardedFor;
+            if (headers.TryGetValue("X-Forwarded-For", out forwardedFor))
+            {
+                if (!string.IsNullOrWhiteSpace(forwardedFor))
+                    return forwardedFor;
+            }
+            return null;
+        }
+    }
+}
---
+++ src/Tests/Gate.Middleware.Tests/ForwardedForTests.cs
@@ -0,0 +1,68 @@
+﻿using Gate.TestHelpers;
+using NUnit.Framework;
+
+namespace Gate.Middleware.Tests
+{
+    public class ForwardedForTests
+    {
+        static FakeHostResponse CallPipeline(string forwardedFor, string clientIp, params string[] knownHttpProxies)
+        {
+            return AppUtils.CallPipe(
+                builder => builder.Use(ForwardedFor.Middleware, knownHttpProxies).Run(AppUtils.ShowEnvironment),
+                request =>
+                {
+                    if (clientIp != null)
+                        request["server.CLIENT_IP"] = clientIp;
+                    if (forwardedFor != null)
+                        request.Headers["X-Forwarded-For"] = forwardedFor;
+                });
+        }
+
+        static void AssertResultValues(FakeHostResponse result, string expectedForwardedFor, string expectedClientIp)
+        {
+            var elt = result.BodyXml.Element("server.CLIENT_IP");
+            var clientIp = elt == null ? null : elt.Value;
+
+
+            elt = result.BodyXml.Element("headers").Element("X-Forwarded-For");
+            var forwardedFor = elt == null ? null : elt.Value;
+
+            Assert.That(clientIp, Is.EqualTo(expectedClientIp));
+            Assert.That(forwardedFor, Is.EqualTo(expectedForwardedFor));
+        }
+
+        [Test]
+        public void ClientIp_becomes_ForwardedFor_when_XFF_absent()
+        {
+            var result = CallPipeline(null, "1.2.3.4");
+            AssertResultValues(result, "1.2.3.4", null);
+        }
+
+        [Test]
+        public void ClientIp_added_to_of_forwardedFor_if_XFF_present()
+        {
+            var result = CallPipeline("6.5.7.8, 9.10.11.12", "1.2.3.4");
+            AssertResultValues(result, "6.5.7.8, 9.10.11.12,1.2.3.4", null);
+        }
+
+
+        [Test]
+        public void Known_proxy_address_is_removed_from_client_ip()
+        {
+            var result = CallPipeline("6.5.7.8", "127.0.0.1", "127.0.0.1");
+            AssertResultValues(result, "6.5.7.8", null);
+        }
+        [Test]
+        public void Multiple_proxy_addresses_are_pulled_as_long_as_they_are_recognized()
+        {
+            var result = CallPipeline("6.5.7.8, 9.10.11.12, 13.14.15.16", "127.0.0.1", "127.0.0.1", "9.10.11.12", "13.14.15.16");
+            AssertResultValues(result, "6.5.7.8", null);
+        }
+        [Test]
+        public void Spoofed_remote_ips_are_left_on_the_header_when_supposed_proxies_are_not_recognized()
+        {
+            var result = CallPipeline("255.255.255.255, 6.5.7.8, 9.10.11.12, 13.14.15.16", "127.0.0.1", "127.0.0.1", "9.10.11.12", "13.14.15.16");
+            AssertResultValues(result, "255.255.255.255,6.5.7.8", null);
+        }
+    }
+}
