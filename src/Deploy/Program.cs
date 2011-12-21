25b9933ab91e7f7afd2b27f6b69683803fb1bf3e
Louis DeJardin <louis.dejardin@gmail.com>
2011-12-20 18:51:14 -0800

    Adding Deploy project to compile packages

---
+++ src/Deploy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Packages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This project is a placeholder to gather and package solution artifacts.");
+        }
+    }
+}
