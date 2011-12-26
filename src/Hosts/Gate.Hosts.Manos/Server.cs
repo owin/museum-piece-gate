d7654f19fd644ad3a854b76bba32ec032c59493e
Louis DeJardin <louis.dejardin@gmail.com>
2011-12-25 17:41:32 -0800

    Empty projects to contain Manos http handler
    Another managed-code web server

---
+++ src/Hosts/Gate.Hosts.Manos/Server.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Configuration;
+using Gate.Builder;
+using Gate.Owin;
+
+namespace Gate.Hosts.Manos
+{
+    public static class Server
+    {
+        public static IDisposable Create(int port)
+        {
+            return Create(port, "");
+        }
+
+        public static IDisposable Create(int port, string path)
+        {
+            return Create(ConfigurationManager.AppSettings["Gate.Startup"], port, path);
+        }
+
+        public static IDisposable Create(string startupName, int port)
+        {
+            return Create(startupName, port, "");
+        }
+
+        public static IDisposable Create(string startupName, int port, string path)
+        {
+            AppDelegate app = AppBuilder.BuildConfiguration(startupName);
+            return Create(app, port, path);
+        }
+
+        public static IDisposable Create(AppDelegate app, int port)
+        {
+            return Create(app, port, "");
+        }
+        public static IDisposable Create(AppDelegate app, int port, string path)
+        {
+            return new Disposable(() => { });
+        }
+
+
+        public class Disposable : IDisposable
+        {
+            readonly Action _dispose;
+
+            public Disposable(Action dispose)
+            {
+                
+
+
+                _dispose = dispose;
+            }
+
+            public void Dispose()
+            {
+                _dispose();
+            }
+        }
+    }
+}
---
+++ src/Tests/Gate.Hosts.Manos.Tests/ServerTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using Gate.Helpers;
+using NUnit.Framework;
+
+namespace Gate.Hosts.Manos.Tests
+{
+    [TestFixture]
+    public class ServerTests
+    {
+        [Test]
+        public void ServerCanBeCreatedAndDisposed()
+        {
+            var server = Server.Create((env, result, fault) => { throw new NotImplementedException(); }, 8090, "");
+            server.Dispose();
+        }
+
+        [Test]
+        public void ServerWillRespondToRequests()
+        {
+            using (Server.Create(Wilson.App(), 8090))
+            {
+                var request = (HttpWebRequest)WebRequest.Create("http://localhost:8090");
+                using (var response = (HttpWebResponse)request.GetResponse())
+                {
+                    using (var reader = new StreamReader(response.GetResponseStream()))
+                    {
+                        var text = reader.ReadToEnd();
+                        Assert.That(text, Is.StringContaining("hello world"));
+                    }
+                }
+            }
+        }
+    }
+}
