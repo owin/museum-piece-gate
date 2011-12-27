7401929a621f8b999f3b5de6de9101b037b65339
Louis DeJardin <louis.dejardin@gmail.com>
2011-12-27 12:10:08 -0800

    Working around a compiler bug

---
+++ src/Tests/Gate.HttpListener.Tests/Workaround.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Gate.Owin;
+
+namespace Gate.HttpListener.Tests
+{
+    class Workaround
+    {
+        ResultDelegate _resultDelegate;
+        Func<int, int, int> _func3;
+    }
+}
