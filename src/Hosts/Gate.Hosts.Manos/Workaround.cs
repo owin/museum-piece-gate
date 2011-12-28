fe0c99182253fbbf229ac4bec8ec78942a5ad1d3
Louis DeJardin <louis.dejardin@gmail.com>
2011-12-27 20:24:34 -0800

    Adding workaround for compiler delegate bug.

---
+++ src/Hosts/Gate.Hosts.Manos/Workaround.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Gate.Owin;
+
+namespace Gate.Hosts.Manos
+{
+    class Workaround
+    {
+#pragma warning disable 169
+        ResultDelegate _resultDelegate;
+        Func<int, int, int> _func3;
+#pragma warning restore 169
+    }
+}
---
+++ src/Hosts/Gate.HttpListener/Workaround.cs
@@ -0,0 +1,11 @@
+﻿using Gate.Owin;
+
+namespace Gate.HttpListener
+{
+    class Workaround
+    {
+#pragma warning disable 169
+        ResultDelegate _resultDelegate;
+#pragma warning restore 169
+    }
+}
