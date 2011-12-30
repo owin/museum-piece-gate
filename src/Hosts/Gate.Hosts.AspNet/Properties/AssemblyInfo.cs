0eb64aa15de9693072d69c0c51a43603a98e752a
Louis DeJardin <louis.dejardin@gmail.com>
2011-12-29 23:52:40 -0800

    Renaming Gate.AspNet to Gate.Hosts.AspNet

--- src/Hosts/Gate.Hosts.AspNet/Properties/AssemblyInfo.cs
+++ src/Hosts/Gate.Hosts.AspNet/Properties/AssemblyInfo.cs
@@ -1,8 +1,8 @@
 using System.Runtime.CompilerServices;
 using System.Reflection;
 using System.Runtime.InteropServices;
-[assembly: AssemblyTitle("Gate.AspNet")]
-[assembly: AssemblyProduct("Gate.AspNet")]
+[assembly: AssemblyTitle("Gate.Hosts.AspNet")]
+[assembly: AssemblyProduct("Gate.Hosts.AspNet")]
 [assembly: AssemblyVersion("0.2.0")]
 [assembly: AssemblyFileVersion("0.2.0")]
 
