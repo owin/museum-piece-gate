235b54910ab1bcad1ccc958a48e2a68330f89850
Louis DeJardin <louis.dejardin@gmail.com>
2012-01-08 18:04:20 -0800

    Trying to put together some "Stack" nuget packages

---
+++ src/Deploy/Stack.HttpListener.Nancy/Example-Startup.cs.pp
@@ -0,0 +1,22 @@
+﻿using Gate.Adapters.Nancy;
+using Gate.Middleware;
+using Gate.Owin;
+
+namespace $rootnamespace$
+{
+    public static class Startup
+    {
+        public static void Configuration(IAppBuilder builder)
+        {
+            builder
+                .RunNancy();
+        }
+
+        public static void Debug(IAppBuilder builder)
+        {
+            builder
+                .UseShowExceptions()
+                .RunNancy();
+        }
+    }
+}
