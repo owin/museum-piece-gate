0d17d708743649ebec3efd0f0c8f291deb4e030e
Louis DeJardin <louis.dejardin@gmail.com>
2012-01-28 14:40:32 -0800

    Using NDesk.Options for commandline

--- src/Main/Ghost.Engine/CommandLine/Program.cs
+++ src/Main/Ghost.Engine/CommandLine/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using Ghost.Engine.Settings;
+using NDesk.Options;
 
 namespace Ghost.Engine.CommandLine
 {
@@ -8,20 +10,9 @@ namespace Ghost.Engine.CommandLine
     {
         public static void Main(string[] args)
         {
-            Arguments arguments;
-            try
+            var arguments = ParseArguments(args);
+            if (arguments == null)
             {
-                arguments = ParseArguments(args);
-            }
-            catch (Exception ex)
-            {
-                Console.Error.WriteLine(ex.Message);
-                ShowHelp(new string[0]);
-                return;
-            }
-            if (arguments.ShowHelp)
-            {
-                ShowHelp(arguments.HelpArgs);
                 return;
             }
 
@@ -31,11 +22,6 @@ namespace Ghost.Engine.CommandLine
                 Console.WriteLine("Press enter to exit");
                 Console.ReadLine();
             }
-        }
-        private static Arguments ParseArguments(IEnumerable<string> args)
-        {
-            var parser = new Parser();
-            return parser.Parse(args);
         }
 
         private static IGhostEngine BuildEngine()
@@ -59,6 +45,82 @@ namespace Ghost.Engine.CommandLine
             });
         }
 
+        private static Arguments ParseArguments(IEnumerable<string> args)
+        {
+            var arguments = new Arguments();
+            var optionSet = new OptionSet()
+                .Add(
+                    "s=|server=", 
+                    @"Load assembly named ""Gate.Hosts.TYPE.dll"" to determine http server to use. TYPE defaults to HttpListener.", 
+                    x => arguments.Server = x)
+                .Add(
+                    "u=|url=", 
+                    @"May be used to set --scheme, --host, --port, and --path options with a combined URIPREFIX value. Format is '<scheme>://<host>[:<port>]<path>/'.", 
+                    x => arguments.Url = x)
+                .Add(
+                    "S=|scheme=", 
+                    @"Determine which socket protocol server should bind with. SCHEME may be 'http' or 'https'. Defaults to 'http'.", 
+                    x => arguments.Scheme = x)
+                .Add(
+                    "h=|host=", 
+                    @"Which host name or IP address to listen on. NAME defaults to '+' for all IP addresses.", 
+                    x => arguments.Host = x)
+                .Add(
+                    "p=|port=", 
+                    @"Which TCP port to listen on. NUMBER defaults to 8080.", 
+                    (int x) => arguments.Port = x)
+                .Add(
+                    "P=|path=", 
+                    @"Determines the virtual directory to run use as the base path for <application> requests. PATH must start with a '/'.", 
+                    x => arguments.Path = x)
+                .Add(
+                    "?|help", 
+                    @"Show this message and exit.", 
+                    x => arguments.ShowHelp = x != null)
+                ;
+
+            List<string> extra;
+            try
+            {
+                extra = optionSet.Parse(args);
+            }
+            catch (OptionException e)
+            {
+                Console.Write("Ghost: ");
+                Console.WriteLine(e.Message);
+                Console.WriteLine("Try 'Ghost --help' for more information.");
+                return null;
+            }
+            if (arguments.ShowHelp)
+            {
+                ShowHelp(optionSet, extra);
+                return null;
+            }
+            arguments.Startup = string.Join(" ", extra.ToArray());
+            return arguments;
+        }
+
+        private static void ShowHelp(OptionSet optionSet, IEnumerable<string> helpArgs)
+        {
+            Console.Write(
+@"Usage: Ghost [options] [<application>]
+Runs <application> on an http server
+Example: Ghost -p8080 HelloWorld.Startup
+
+Options:
+");
+            optionSet.WriteOptionDescriptions(Console.Out);
+            Console.Write(
+@"
+Environment Variables:
+PORT                         Changes the default TCP port to listen on when 
+                               both --port and --url options are not provided.
+OWIN_SERVER                  Changes the default server TYPE to use when
+                               the --server option is not provided.
+
+");
+        }
+
         private static void TakeDefaultsFromEnvironment(GhostSettings settings)
         {
             var port = Environment.GetEnvironmentVariable("PORT", EnvironmentVariableTarget.Process);
@@ -69,37 +131,6 @@ namespace Ghost.Engine.CommandLine
             var owinServer = Environment.GetEnvironmentVariable("OWIN_SERVER", EnvironmentVariableTarget.Process);
             if (!string.IsNullOrWhiteSpace(owinServer))
                 settings.DefaultServer = owinServer;
-        }
-
-        private static void ShowHelp(IEnumerable<string> helpArgs)
-        {
-            Console.Write(
-@"Usage: Ghost [options] [<application>]
-Runs <application> on an http server
-Example: Ghost -p8080 HelloWorld.Startup
-
-Options:
- /s, --server=TYPE      Load assembly named ""Gate.Hosts.TYPE.dll"" to determine
-                        http server to use. TYPE defaults to HttpListener.
- /u, --url=URIPREFIX    May be used to set --scheme, --host, --port, and 
-                        --path options with a combined URIPREFIX value.
-                        Format is '<scheme>://<host>[:<port>]<path>/'.
- /S, --scheme=SCHEME    Determine which socket protocol server should bind with.
-                        SCHEME may be 'http' or 'https'. Defaults to 'http'.
- /h, --host=NAME        Which host name or IP address to listen on. 
-                        NAME defaults to '+' for all IP addresses.
- /p, --port=NUMBER      Which TCP port to listen on. NUMBER defaults to 8080.
- /P, --path=PATH        Determines the virtual directory to run use as the
-                        base path for <application> requests. PATH must start 
-                        with a '/'.
-
-Environment Variables:
-PORT                    Changes the default TCP port to listen on when 
-                        both --port and --url options are not provided.
-OWIN_SERVER             Changes the default server TYPE to use when
-                        the --server option is not provided.
-
-");
         }
     }
 }
