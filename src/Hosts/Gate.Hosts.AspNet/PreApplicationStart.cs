932e9f90f75b5c22f5b62688cf32303fe8508277
Louis DeJardin <louis.dejardin@gmail.com>
2012-02-08 20:17:42 -0800

    Making AspNet module autoregister where appropriate
    Use appSettings "Gate.Hosts.AspNet.PreApplicationStart" value
    of "Disabled" to avoid this behavior.

---
+++ src/Hosts/Gate.Hosts.AspNet/PreApplicationStart.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Configuration;
+using System.Web;
+using Gate.Hosts.AspNet;
+using Microsoft.Web.Infrastructure.DynamicModuleHelper;
+
+[assembly: PreApplicationStartMethod(typeof(PreApplicationStart), "Initialize")]
+
+namespace Gate.Hosts.AspNet
+{
+    public static class PreApplicationStart
+    {
+        public static void Initialize()
+        {
+            try
+            {
+                var appSetting = ConfigurationManager.AppSettings["Gate.Hosts.AspNet.PreApplicationStart"];
+                if (string.IsNullOrWhiteSpace(appSetting) ||
+                    string.Equals("Enabled", appSetting, StringComparison.InvariantCultureIgnoreCase))
+                {
+
+                    DynamicModuleUtility.RegisterModule(typeof(Module));
+                }
+            }
+            // ReSharper disable EmptyGeneralCatchClause
+            catch
+            {
+                // If we're unable to load MWI then just swallow the exception and don't allow
+                // the automagic hub registration
+            }
+            // ReSharper restore EmptyGeneralCatchClause
+        }
+    }
+}
