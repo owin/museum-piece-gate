0a42efd7b41755d83e940accdcda4687fbd1e49b
Marco Cunha <marco.cunha@gmail.com>
2012-03-21 14:55:29 +0100

    Don't schedule new job if there's nothing to drain

--- src/Hosts/Gate.Hosts.Kayak/SchedulerMiddleware.cs
+++ src/Hosts/Gate.Hosts.Kayak/SchedulerMiddleware.cs
@@ -66,7 +66,7 @@ namespace Gate.Hosts.Kayak
                         {
                             theScheduler.Post(() =>
                             {
-                                if (!flush(() => theScheduler.Post(drained)))
+                                if (!flush(() => if (drained != null) theScheduler.Post(drained)))
                                     if (drained != null) drained.Invoke();
                             });
                             return true;
