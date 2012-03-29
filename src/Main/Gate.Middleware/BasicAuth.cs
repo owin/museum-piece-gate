c545fab02ad72b56ba7100a0287fdd77f5804369
Kevin Swiber <kswiber@gmail.com>
2012-03-28 22:59:25 -0400

    Adding basic auth middleware.

---
+++ src/Main/Gate.Middleware/BasicAuth.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Owin;
+
+namespace Gate.Middleware
+{
+    public static class BasicAuth
+    {
+        public static IAppBuilder UseBasicAuth(this IAppBuilder builder, string realm, Func<string, string, bool> authenticator)
+        {
+            return builder.Use(Middleware, realm, authenticator);
+        }
+
+        public static AppDelegate Middleware(AppDelegate app, string realm, Func<string, string, bool> authenticator)
+        {
+            return (env, result, fault) =>
+            {
+                if ((!env.ContainsKey("gate.RemoteUser") || env["gate.RemoteUser"] == null))
+                {
+                    Authenticate(app, env, result, fault, realm, authenticator);
+                }
+                else
+                {
+                    // Should be authenticated.  Move along.
+                    if (app != null)
+                    {
+                        app(env, result, fault);
+                    }
+                }
+            };
+        }
+
+        private static void Authenticate(AppDelegate app, 
+            IDictionary<string, object> env, ResultDelegate result, Action<Exception> fault,
+            string realm, Func<string, string, bool> authenticator)
+        {
+            const string authorizationKey = "Authorization";
+            var environment = new Environment(env);
+
+            if (!environment.Headers.ContainsKey(authorizationKey))
+            {
+                Unauthorized(result, realm);
+                return;
+            }
+
+            var authorizationHeader = environment.Headers[authorizationKey].ToArray().First();
+            var headerValues = authorizationHeader.Split(new[] {' '}, 2);
+            var scheme = headerValues[0];
+            var param = headerValues[1];
+
+            if (!scheme.Equals("Basic", StringComparison.InvariantCultureIgnoreCase))
+            {
+                result("400 Bad Request", null, null);
+                return;
+            }
+
+            var credentials = Encoding.ASCII.GetString(Convert.FromBase64String(param)).Split(new[] {':'}, 2);
+            var username = credentials[0];
+            var password = credentials[1];
+
+            if (authenticator(username, password))
+            {
+                env["gate.RemoteUser"] = username;
+                app(env, result, fault);
+                return;
+            }
+
+            Unauthorized(result, realm);
+        }
+
+        private static void Unauthorized(ResultDelegate result, string realm)
+        {
+            const string status = "401 Unauthorized";
+            var headers = Headers.New();
+            var challenge = string.Format("Basic realm=\"{0}\"", realm);
+            headers.Add("WWW-Authenticate", new[] { challenge });
+
+            result(status, headers, null);
+        }
+    }
+}
---
+++ src/Tests/Gate.Middleware.Tests/BasicAuthTests.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Gate.Builder;
+using Gate.TestHelpers;
+using NUnit.Framework;
+using Owin;
+
+namespace Gate.Middleware.Tests
+{
+    public class BasicAuthTests
+    {
+        private FakeHost _host;
+
+        [SetUp]
+        public void Setup()
+        {
+            var app = new FakeApp("200 OK");
+
+            Action<IAppBuilder> pipeline = b => b
+                .UseBasicAuth("Howdy", (username, password) => password == "Howdy")
+                .Run(app.AppDelegate);
+
+            var config = AppBuilder.BuildConfiguration(pipeline);
+            _host = new FakeHost(config);
+        }
+
+        [Test]
+        public void Basic_auth_presents_challenge_when_no_credentials_are_provided()
+        {
+            Request(AssertBasicAuthChallenge);
+        }
+
+        [Test]
+        public void Basic_auth_rechallenges_with_incorrect_credentials()
+        {
+            RequestWithBasicAuth("joe", "password", AssertBasicAuthChallenge);
+        }
+
+        [Test]
+        public void Basic_auth_returns_ok_if_credentials_are_valid()
+        {
+            RequestWithBasicAuth("Boy", "Howdy", response => Assert.That(response.Status, Is.EqualTo("200 OK")));
+        }
+
+        [Test]
+        public void Basic_auth_returns_bad_request_when_different_auth_scheme_is_used()
+        {
+            Request(new Dictionary<string, IEnumerable<string>> {{"Authorization", new[] {"Digest blah"}}}, 
+                response => Assert.That(response.Status, Is.EqualTo("400 Bad Request")))
+            ;
+        }
+
+        private void AssertBasicAuthChallenge(FakeHostResponse response)
+        {
+            Assert.That(response.Status, Is.EqualTo("401 Unauthorized"));
+            Assert.That(response.Headers.Keys, Contains.Item("WWW-Authenticate"));
+            Assert.That(response.Headers["WWW-Authenticate"], Contains.Item("Basic realm=\"Howdy\""));
+            Assert.That(response.Body, Is.Null);
+        }
+
+        private void RequestWithBasicAuth(string username, string password, Action<FakeHostResponse> responseAction = null)
+        {
+            var base64 = Convert.ToBase64String(Encoding.ASCII.GetBytes(username + ":" + password));
+
+            Request(new Dictionary<string, IEnumerable<string>> { { "Authorization", new[] { "Basic " + base64 } } }, responseAction);
+        }
+
+        private void Request(Action<FakeHostResponse> responseAction)
+        {
+            Request(new Dictionary<string, IEnumerable<string>>(), responseAction);
+        }
+
+        private void Request(Dictionary<string, IEnumerable<string>> headers, Action<FakeHostResponse> responseAction = null)
+        {
+            if (responseAction != null)
+            {
+                responseAction(_host.GET("/", request => request.Headers = headers));
+            }
+        }
+    }
+}
