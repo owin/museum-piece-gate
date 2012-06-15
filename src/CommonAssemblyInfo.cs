aba21a8254f067e69c23ade784c8469f9bf1d077
Louis DeJardin <louis.dejardin@gmail.com>
2012-06-15 16:48:36 -0700

    Another attempt to have nuget friendly build number

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -7,10 +7,10 @@
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("0.5")]
+// [assembly: AssemblyVersion("0.6")]
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.5")]
-[assembly: AssemblyFileVersion("0.5")]
-[assembly: AssemblyInformationalVersion("0.5-ctp1")]
+[assembly: AssemblyVersion("0.6")]
+[assembly: AssemblyFileVersion("0.6")]
+[assembly: AssemblyInformationalVersion("0.6-ctp1")]
