93ffde099bb774a65908db19fbd6f5fd23873a2d
Louis DeJardin <louis.dejardin@gmail.com>
2012-06-25 15:59:26 -0700

    Updating Gate.Builder for ctp2

--- src/Main/Gate.Builder/Adapters.cs
+++ src/Main/Gate.Builder/Adapters.cs
@@ -6,110 +6,119 @@ using Owin;
 
 namespace Gate.Builder
 {
-    using ResultTuple = Tuple<string, IDictionary<String, string[]>, BodyDelegate>;
+    //    using ResultTuple = Tuple<string, IDictionary<String, string[]>, BodyDelegate>;
 
-#pragma warning disable 811
-    using AppAction = Action< // app
-        IDictionary<string, object>, // env
-        Action< // result
-            string, // status
-            IDictionary<string, string[]>, // headers
-            Action< // body
-                Func< // write
-                    ArraySegment<byte>, // data                     
-                    Action, // continuation
-                    bool>, // buffering
-                Action< // end
-                    Exception>, // error
-                CancellationToken>>, // cancel
-        Action<Exception>>; // error
+    //#pragma warning disable 811
+    //    using AppAction = Action< // app
+    //        IDictionary<string, object>, // env
+    //        Action< // result
+    //            string, // status
+    //            IDictionary<string, string[]>, // headers
+    //            Action< // body
+    //                Func< // write
+    //                    ArraySegment<byte>, // data                     
+    //                    Action, // continuation
+    //                    bool>, // buffering
+    //                Action< // end
+    //                    Exception>, // error
+    //                CancellationToken>>, // cancel
+    //        Action<Exception>>; // error
 
-    using BodyAction = Action< // body
-        Func< // write
-            ArraySegment<byte>, // data                     
-            Action, // continuation
-            bool>, // buffering
-        Action< // end
-            Exception>, // error
-        CancellationToken>; //cancel
+    //    using BodyAction = Action< // body
+    //        Func< // write
+    //            ArraySegment<byte>, // data                     
+    //            Action, // continuation
+    //            bool>, // buffering
+    //        Action< // end
+    //            Exception>, // error
+    //        CancellationToken>; //cancel
 
 
     public static class Adapters
     {
-        public static AppAction ToAction(AppDelegate app)
-        {
-            return
-                (env, result, fault) =>
-                {
-                    var revert = Replace<BodyAction, BodyDelegate>(env, ToDelegate);
-                    app(
-                        env,
-                        (status, headers, body) =>
-                        {
-                            revert();
-                            result(status, headers, ToAction(body));
-                        },
-                        ex =>
-                        {
-                            revert();
-                            fault(ex);
-                        });
-                };
-        }
+        //public static AppAction ToAction(AppDelegate app)
+        //{
+        //    return
+        //        (env, result, fault) =>
+        //        {
+        //            var revert = Replace<BodyAction, BodyDelegate>(env, ToDelegate);
+        //            app(
+        //                env,
+        //                (status, headers, body) =>
+        //                {
+        //                    revert();
+        //                    result(status, headers, ToAction(body));
+        //                },
+        //                ex =>
+        //                {
+        //                    revert();
+        //                    fault(ex);
+        //                });
+        //        };
+        //}
 
-        public static AppDelegate ToDelegate(AppAction app)
-        {
-            return
-                (env, result, fault) =>
-                {
-                    var revert = Replace<BodyDelegate, BodyAction>(env, ToAction);
-                    app(
-                        env,
-                        (status, headers, body) =>
-                        {
-                            revert();
-                            result(status, headers, ToDelegate(body));
-                        },
-                        ex =>
-                        {
-                            revert();
-                            fault(ex);
-                        });
-                };
-        }
+        //public static AppDelegate ToDelegate(AppAction app)
+        //{
+        //    return
+        //        (env, result, fault) =>
+        //        {
+        //            var revert = Replace<BodyDelegate, BodyAction>(env, ToAction);
+        //            app(
+        //                env,
+        //                (status, headers, body) =>
+        //                {
+        //                    revert();
+        //                    result(status, headers, ToDelegate(body));
+        //                },
+        //                ex =>
+        //                {
+        //                    revert();
+        //                    fault(ex);
+        //                });
+        //        };
+        //}
 
-        static Action Replace<TFrom, TTo>(IDictionary<string, object> env, Func<TFrom, TTo> adapt)
-        {
-            object body;
-            if (env.TryGetValue(OwinConstants.RequestBody, out body) && body is TFrom)
-            {
-                env[OwinConstants.RequestBody] = adapt((TFrom)body);
-                return () => env[OwinConstants.RequestBody] = body;
-            }
-            return () => { };
-        }
+        //static Action Replace<TFrom, TTo>(IDictionary<string, object> env, Func<TFrom, TTo> adapt)
+        //{
+        //    object body;
+        //    if (env.TryGetValue(OwinConstants.RequestBody, out body) && body is TFrom)
+        //    {
+        //        env[OwinConstants.RequestBody] = adapt((TFrom)body);
+        //        return () => env[OwinConstants.RequestBody] = body;
+        //    }
+        //    return () => { };
+        //}
 
-        public static BodyAction ToAction(BodyDelegate body)
-        {
-            return (write, end, cancel) => body(write, end, cancel);
-        }
+        //public static BodyAction ToAction(BodyDelegate body)
+        //{
+        //    return (write, end, cancel) => body(write, end, cancel);
+        //}
 
-        public static BodyDelegate ToDelegate(BodyAction body)
-        {
-            return (write, end, cancel) => body(write, end, cancel);
-        }
+        //public static BodyDelegate ToDelegate(BodyAction body)
+        //{
+        //    return (write, end, cancel) => body(write, end, cancel);
+        //}
 
 
         public static AppTaskDelegate ToTaskDelegate(AppDelegate app)
         {
             return
-                env =>
+                call =>
                 {
-                    var tcs = new TaskCompletionSource<ResultTuple>();
+                    var tcs = new TaskCompletionSource<ResultParameters>();
                     app(
-                        env,
-                        (status, headers, body) => tcs.SetResult(new ResultTuple(status, headers, body)),
-                        tcs.SetException);
+                        call,
+                        (result, exception) =>
+                        {
+                            if (exception == null)
+                            {
+                                tcs.SetResult(result);
+                            }
+                            else
+                            {
+                                tcs.SetException(exception);
+                            }
+                        });
                     return tcs.Task;
                 };
         }
@@ -117,16 +126,14 @@ namespace Gate.Builder
         public static AppDelegate ToDelegate(AppTaskDelegate app)
         {
             return
-                (env, result, fault) =>
-                {
-                    var task = app(env);
-                    task.ContinueWith(
-                        t => result(t.Result.Item1, t.Result.Item2, t.Result.Item3),
-                        TaskContinuationOptions.ExecuteSynchronously | TaskContinuationOptions.OnlyOnRanToCompletion);
-                    task.ContinueWith(
-                        t => fault(t.Exception),
-                        TaskContinuationOptions.ExecuteSynchronously | TaskContinuationOptions.OnlyOnFaulted);
-                };
+                (call, callback) =>
+                    app(call)
+                        .Then(result => callback(result, null))
+                        .Catch(caught =>
+                        {
+                            callback(default(ResultParameters), caught.Exception);
+                            return caught.Handled();
+                        });
         }
     }
 }
