f33a473c80b10dba460b67609accbc1c43dcf123
Tratcher <chrross@CHRROSS-DEV.redmond.corp.microsoft.com>
2012-08-23 13:13:49 -0700

    Removing obsolete hosts, tests, and samples.

--- src/Hosts/Gate.Hosts.AspNet/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-using System.Runtime.CompilerServices;
-using System.Reflection;
-using System.Runtime.InteropServices;
-[assembly: AssemblyTitle("Gate.Hosts.AspNet")]
-[assembly: AssemblyProduct("Gate.Hosts.AspNet")]
-[assembly: InternalsVisibleTo("Gate.AspNet.Tests")]
-
