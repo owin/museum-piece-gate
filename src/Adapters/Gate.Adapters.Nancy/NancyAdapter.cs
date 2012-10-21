26b8ea8ae912110a06fdcc44749ee34272385fed
unknown <lodejard@LODEJARD21.redmond.corp.microsoft.com>
2012-10-21 15:44:44 -0400

    Some work for MonkeySpace demo
    Bumping version
    Moving more IAppBuilder extension methods into Owin namespace

--- src/Adapters/Gate.Adapters.Nancy/NancyAdapter.cs
+++ src/Adapters/Gate.Adapters.Nancy/NancyAdapter.cs
@@ -4,76 +4,103 @@ using System.Globalization;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Gate.Adapters.Nancy;
 using Owin;
 using Nancy;
 using Nancy.Bootstrapper;
 using Nancy.IO;
 using System.IO;
 
+namespace Owin
+{
+    public static class NancyExtensions
+    {
+        public static void RunNancy(this IAppBuilder builder)
+        {
+            builder.Run(new NancyAdapter(null, NancyBootstrapperLocator.Bootstrapper));
+        }
+
+        public static void RunNancy(this IAppBuilder builder, INancyBootstrapper bootstrapper)
+        {
+            builder.Run(new NancyAdapter(null, bootstrapper));
+        }
+
+        public static IAppBuilder UseNancy(this IAppBuilder builder)
+        {
+            return builder.UseType<NancyAdapter>(NancyBootstrapperLocator.Bootstrapper);
+        }
+
+        public static IAppBuilder UseNancy(this IAppBuilder builder, INancyBootstrapper bootstrapper)
+        {
+            return builder.UseType<NancyAdapter>(bootstrapper);
+        }
+    }
+}
+
 namespace Gate.Adapters.Nancy
 {
     using AppFunc = Func<IDictionary<string, object>, Task>;
 
-    public static class NancyAdapter
+    public class NancyAdapter
     {
-        public static IAppBuilder RunNancy(this IAppBuilder builder)
-        {
-            return builder.UseFunc<AppFunc>(_ => App());
-        }
+        readonly AppFunc _next;
+        readonly INancyEngine _engine;
 
-        public static IAppBuilder RunNancy(this IAppBuilder builder, INancyBootstrapper bootstrapper)
-        {
-            return builder.UseFunc<AppFunc>(_ => App(bootstrapper));
-        }
-
-        public static AppFunc App()
-        {
-            return App(NancyBootstrapperLocator.Bootstrapper);
-        }
-
-        public static AppFunc App(INancyBootstrapper bootstrapper)
+        public NancyAdapter(AppFunc next, INancyBootstrapper bootstrapper)
         {
             bootstrapper.Initialise();
-            var engine = bootstrapper.GetEngine();
-            return env =>
+
+            _next = next;
+            _engine = bootstrapper.GetEngine();
+        }
+
+        public Task Invoke(IDictionary<string, object> env)
+        {
+            var owinRequestMethod = Get<string>(env, OwinConstants.RequestMethod);
+            var owinRequestScheme = Get<string>(env, OwinConstants.RequestScheme);
+            var owinRequestHeaders = Get<IDictionary<string, string[]>>(env, OwinConstants.RequestHeaders);
+            var owinRequestPathBase = Get<string>(env, OwinConstants.RequestPathBase);
+            var owinRequestPath = Get<string>(env, OwinConstants.RequestPath);
+            var owinRequestQueryString = Get<string>(env, OwinConstants.RequestQueryString);
+            var owinRequestBody = Get<Stream>(env, OwinConstants.RequestBody);
+            var serverClientIp = Get<string>(env, OwinConstants.RemoteIpAddress);
+
+            var owinResponseHeaders = Get<IDictionary<string, string[]>>(env, OwinConstants.ResponseHeaders);
+            var owinResponseBody = Get<Stream>(env, OwinConstants.ResponseBody);
+
+            var url = new Url
             {
-                var owinRequestMethod = Get<string>(env, OwinConstants.RequestMethod);
-                var owinRequestScheme = Get<string>(env, OwinConstants.RequestScheme);
-                var owinRequestHeaders = Get<IDictionary<string, string[]>>(env, OwinConstants.RequestHeaders);
-                var owinRequestPathBase = Get<string>(env, OwinConstants.RequestPathBase);
-                var owinRequestPath = Get<string>(env, OwinConstants.RequestPath);
-                var owinRequestQueryString = Get<string>(env, OwinConstants.RequestQueryString);
-                var owinRequestBody = Get<Stream>(env, OwinConstants.RequestBody);
-                var serverClientIp = Get<string>(env, OwinConstants.RemoteIpAddress);
+                Scheme = owinRequestScheme,
+                HostName = GetHeader(owinRequestHeaders, "Host"),
+                Port = null,
+                BasePath = owinRequestPathBase,
+                Path = owinRequestPath,
+                Query = owinRequestQueryString,
+            };
 
-                var owinResponseHeaders = Get<IDictionary<string, string[]>>(env, OwinConstants.ResponseHeaders);
-                var owinResponseBody = Get<Stream>(env, OwinConstants.ResponseBody);
+            var body = new RequestStream(owinRequestBody, ExpectedLength(owinRequestHeaders), false);
 
-                var url = new Url
+            var nancyRequest = new Request(
+                owinRequestMethod,
+                url,
+                body,
+                owinRequestHeaders.ToDictionary(kv => kv.Key, kv => (IEnumerable<string>)kv.Value, StringComparer.OrdinalIgnoreCase),
+                serverClientIp);
+
+            var tcs = new TaskCompletionSource<object>();
+            _engine.HandleRequest(
+                nancyRequest,
+                context =>
                 {
-                    Scheme = owinRequestScheme,
-                    HostName = GetHeader(owinRequestHeaders, "Host"),
-                    Port = null,
-                    BasePath = owinRequestPathBase,
-                    Path = owinRequestPath,
-                    Query = owinRequestQueryString,
-                };
+                    var nancyResponse = context.Response;
 
-                var body = new RequestStream(owinRequestBody, ExpectedLength(owinRequestHeaders), false);
-
-                var nancyRequest = new Request(
-                    owinRequestMethod,
-                    url,
-                    body,
-                    owinRequestHeaders.ToDictionary(kv => kv.Key, kv => (IEnumerable<string>)kv.Value, StringComparer.OrdinalIgnoreCase),
-                    serverClientIp);
-
-                var tcs = new TaskCompletionSource<object>();
-                engine.HandleRequest(
-                    nancyRequest,
-                    context =>
+                    if (_next != null && nancyResponse.StatusCode == HttpStatusCode.NotFound)
                     {
-                        var nancyResponse = context.Response;
+                        _next(env).CopyResultToCompletionSource(tcs, null);
+                    }
+                    else
+                    {
+                        env[OwinConstants.ResponseStatusCode] = (int)nancyResponse.StatusCode;
                         foreach (var header in nancyResponse.Headers)
                         {
                             owinResponseHeaders.Add(header.Key, new string[] { header.Value });
@@ -88,14 +115,13 @@ namespace Gate.Adapters.Nancy
                             owinResponseHeaders["Set-Cookie"] = nancyResponse.Cookies.Select(cookie => cookie.ToString()).ToArray();
                         }
 
-                        env[OwinConstants.ResponseStatusCode] = (int)nancyResponse.StatusCode;
                         nancyResponse.Contents(owinResponseBody);
                         context.Dispose();
                         tcs.TrySetResult(null);
-                    },
-                    tcs.SetException);
-                return tcs.Task;
-            };
+                    }
+                },
+                tcs.SetException);
+            return tcs.Task;
         }
 
 
--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -7,10 +7,10 @@
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("0.18")]
+// [assembly: AssemblyVersion("0.19")]
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.18")]
-[assembly: AssemblyFileVersion("0.18")]
-[assembly: AssemblyInformationalVersion("0.18")]
+[assembly: AssemblyVersion("0.19")]
+[assembly: AssemblyFileVersion("0.19")]
+[assembly: AssemblyInformationalVersion("0.19")]
